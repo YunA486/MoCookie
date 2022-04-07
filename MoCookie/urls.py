a5867717282b693ff409d95a3c0de3fbc9d27d90
YunA486 <w2039@e-mirim.hs.kr>
2022-04-07 22:01:58 +0900

    signup & login

--- MoCookie/urls.py
+++ MoCookie/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+
+import account.views
 from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/', include('main.urls')),
+    path('', include('main.urls')),
+    path('login/', account.views.login, name='login'),
+    path('signup/', account.views.signup, name='signup'),
+    path('logout/', account.views.logout, name='logout'),
 ]
